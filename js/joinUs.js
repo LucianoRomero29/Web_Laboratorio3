f7528b9e41c23937f1f252e976086b1716dae2fe
LucianoRomero1 <Lucianooromero1@gmail.com>
2022-05-05 11:59:45 -0300

    Join us y contact us casi terminados

---
+++ js/joinUs.js
@@ -0,0 +1,25 @@
+function showInput(){
+    var div     = document.getElementById("inputJoinUs");
+    var label   = document.getElementById("textOption");
+    var input   = document.getElementById("inputOption");
+    var x       = document.getElementById("selectFindUs").value;
+    if(x == 2 || x == 3 || x == 4){
+        div.style.display = "block";
+        input.required = true;
+        if(x == 2){
+            label.innerHTML = "¿Cuál?"
+        }
+        if(x == 3){
+            label.innerHTML = "Nombre y Apellido del conocido/a"
+        }
+        if(x == 4){
+            label.innerHTML = "Otro"
+        }
+        
+    }
+    else{
+        div.style.display = "none";
+        input.required = false;
+    }
+    
+}
